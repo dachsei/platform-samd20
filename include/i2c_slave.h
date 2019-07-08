815c15c481dd985b1b14cdaeabbf47807d191dec
Philip Dachs <philip.dachs@web.de>
2019-07-08 15:08:03 +0200

    I2C Bugfix AMATCH interrupt and Silicon Errata 1.5.17

--- include/i2c_slave.h
+++ include/i2c_slave.h
@@ -14,6 +14,7 @@ class I2cSlave {
 public:
 	/**
 	 * @param sercom The Sercom interface to use
+	 * @param generatorCore ClockGenerator that should clock this sercom
 	 * @param generator32k A ClockGenerator with a frequency of around 32kHz
 	 * @param addr The address to listen on. Right aligned (without Read/Write bit)
 	 * @param lowTimeout If enabled and SCL is held low for 25ms-35ms, this device will release its clock hold
@@ -22,22 +23,23 @@ public:
 	 *                Use SERCOM_I2CS_CTRLA_SDAHOLD_*_Val Macros
 	 */
 	I2cSlave(
-		Sercom* sercom, const ClockGenerator& generator32k, uint8_t addr,
-		bool lowTimeout = true, unsigned sdaHold = SERCOM_I2CS_CTRLA_SDAHOLD_450_Val
+		Sercom* sercom, const ClockGenerator& generatorCore, const ClockGenerator& generator32k, uint8_t addr,
+		bool lowTimeout = false, unsigned sdaHold = SERCOM_I2CS_CTRLA_SDAHOLD_450_Val
 	)
 		: sercom{sercom}
 	{
 		unsigned sercomIndex = getSercomIndex(sercom);
+		generatorCore.routeToPeripheral(GCLK_CLKCTRL_ID_SERCOM0_CORE_Val + sercomIndex);
 		generator32k.routeToPeripheral(GCLK_CLKCTRL_ID_SERCOMX_SLOW_Val);
 		PM->APBCMASK.reg |= 1 << (PM_APBCMASK_SERCOM0_Pos + sercomIndex);
 
 		sercom->I2CS.CTRLA.reg = SERCOM_I2CS_CTRLA_MODE_I2C_SLAVE
 			| (lowTimeout << SERCOM_I2CS_CTRLA_LOWTOUT_Pos)
 			| SERCOM_I2CS_CTRLA_SDAHOLD(sdaHold);
-		sercom->I2CS.CTRLB.reg = SERCOM_I2CS_CTRLB_AMODE(0);
-		sercom->I2CS.ADDR.reg = SERCOM_I2CS_ADDR_ADDR(addr) | SERCOM_I2CS_ADDR_ADDRMASK(0x7f);
-		sercom->I2CS.INTENSET.reg = SERCOM_I2CS_INTFLAG_DRDY | SERCOM_I2CS_INTFLAG_PREC;
-		sercom->I2CS.INTFLAG.reg = SERCOM_I2CS_INTFLAG_DRDY | SERCOM_I2CS_INTFLAG_PREC;
+		sercom->I2CS.CTRLB.reg = SERCOM_I2CS_CTRLB_AMODE(0) | SERCOM_I2CS_CTRLB_SMEN;
+		sercom->I2CS.ADDR.reg = SERCOM_I2CS_ADDR_ADDR(addr);
+		sercom->I2CS.INTENSET.reg = SERCOM_I2CS_INTFLAG_DRDY | SERCOM_I2CS_INTFLAG_PREC | SERCOM_I2CS_INTFLAG_AMATCH;
+		sercom->I2CS.INTFLAG.reg = SERCOM_I2CS_INTFLAG_DRDY | SERCOM_I2CS_INTFLAG_PREC | SERCOM_I2CS_INTFLAG_AMATCH;
 		auto irq = static_cast<IRQn_Type>(static_cast<unsigned>(SERCOM0_IRQn) + sercomIndex);
 		NVIC_ClearPendingIRQ(irq);
 		NVIC_EnableIRQ(irq);
@@ -54,17 +56,19 @@ public:
 	template <typename FnStop, typename FnWrite, typename FnRead>
 	void interruptHandler(FnStop onStop, FnWrite onWrite, FnRead onRead) const
 	{
-		if (sercom->I2CS.INTFLAG.reg & SERCOM_I2CS_INTFLAG_PREC) {
+		uint8_t intflags = sercom->I2CS.INTFLAG.reg;
+		if (intflags & SERCOM_I2CS_INTFLAG_AMATCH) {
+			sercom->I2CS.INTFLAG.reg = SERCOM_I2CS_INTFLAG_AMATCH;
+		}
+		if (intflags & SERCOM_I2CS_INTFLAG_PREC) {
 			onStop();
 			sercom->I2CS.INTFLAG.reg = SERCOM_I2CS_INTFLAG_PREC;
 		}
-		if (sercom->I2CS.INTFLAG.reg & SERCOM_I2CS_INTFLAG_DRDY) {
+		if (intflags & SERCOM_I2CS_INTFLAG_DRDY) {
 			if (sercom->I2CS.STATUS.bit.DIR == 0) {
 				onWrite(sercom->I2CS.DATA.reg);
-				sercom->I2CS.CTRLB.bit.CMD = 3;
 			} else {
 				sercom->I2CS.DATA.reg = onRead();
-				sercom->I2CS.CTRLB.bit.CMD = 3;
 			}
 		}
 	}
